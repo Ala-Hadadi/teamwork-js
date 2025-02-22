a5e1cd7c39ad65a6b3f14206e320f7896e6c23d2
Ala-Hadadi <alahadadi1384@gmail.com>
2025-02-22 08:09:05 -0800

    adding changes

--- home/1.js
+++ home/1.js
@@ -1,17 +1,17 @@
 function signIn() {
-    alert("Redirecting to Sign In page...");
- 
-    window.location.href = "http://127.0.0.1:5500/sing%20in/3.html";
+  alert("Redirecting to Sign In page...");
+
+  window.location.href = "../signIn/3.html";
 }
 
 function signUp() {
-    alert("Redirecting to Sign Up page...");
-   
-    window.location.href = "http://127.0.0.1:5500/sing%20up/2.html";
+  alert("Redirecting to Sign Up page...");
+
+  window.location.href = "../signUp/2.html";
 }
 
 function viewLeaderboard() {
-    alert("Opening Leaderboard...");
+  alert("Opening Leaderboard...");
 
-    window.location.href = "http://127.0.0.1:5500/scores/index.html";
+  window.location.href = "../scores/index.html";
 }
